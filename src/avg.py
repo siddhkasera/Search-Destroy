7746b1f47e1d39fabb25a638319af743ec785c31
siddhkasera <58352099+siddhkasera@users.noreply.github.com>
2021-04-08 23:06:25 -0400

    Add files via upload
    Average calculations

---
+++ src/avg.py
@@ -0,0 +1,26 @@
+agent1 = [82406, 190218, 47525, 364, 12873, 72380, 3515, 70619, 5252, 172353]
+
+for i in range(len(agent1)):
+    sum = 0
+    sum = sum + agent1[i]
+
+avg = sum / len(agent1)
+print("Average of agent one:", avg)
+
+agent2 = [2870, 957, 32028, 6363, 102024, 213849, 44920, 2973, 66592, 6482
+          ]
+for i in range(len(agent2)):
+    sum = 0
+    sum = sum + agent2[i]
+
+avg = sum / len(agent2)
+print("Average of agent two:", avg)
+
+improved_agent = [22111, 7425, 22966, 27955, 3487, 34391, 39149, 31272, 24453]
+
+for i in range(len(improved_agent)):
+    sum = 0
+    sum = sum + improved_agent[i]
+
+avg = sum / len(improved_agent)
+print("Average for improved agent:", avg)
